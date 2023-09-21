28809756a227d641259af51be674e2bf4f80cd46
Marina Galamdinova <mgalamdinova@gmail.com>
2023-09-21 19:56:56 +0300

    fix: changed protocol for api to https

--- movies/src/utils/MoviesAuth.js
+++ movies/src/utils/MoviesAuth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://api.movies.galamm.nomoredomains.sbs';
+export const BASE_URL = 'https://api.movies.galamm.nomoredomains.sbs';
 // export const BASE_URL = 'http://localhost:3001';
 
 class MoviesAuth {
